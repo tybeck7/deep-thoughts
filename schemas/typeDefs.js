b99288ad374b83c99f8d6fb63df645984e5fc0ce
Tyler Beck <tylerbeck@Tylers-Air.attlocal.net>
2022-05-12 11:54:07 -0700

    reslover and typedef

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -1,8 +1,14 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
 
   type Thought {
     _id: ID
@@ -21,10 +27,11 @@ const typeDefs = gql`
   }
 
   type Query {
+    users: [User]
+    user(username: String!): User
     thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
   }
 `;
 
-
-// export the typeDefs
 module.exports = typeDefs;
